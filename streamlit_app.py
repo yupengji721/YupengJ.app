7886b630ba5437cc60f1c2e7e494f7390b1c7e25
yupengji721 <yupengji721@gmail.com>
2026-07-14 20:01:24 +0000

    Leo

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("🎈 My name is Yupeng!")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/), hello."
-)
